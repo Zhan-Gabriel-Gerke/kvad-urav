e8fcc5490ff08bd6bab6ab14330b0cb224543d54
opilane <opilane@HP-CZC6297B92>
2022-01-11 11:44:37 +0200

    lökl

--- kvad_urav.py
+++ kvad_urav.py
@@ -66,6 +66,56 @@ def veel():
         uvokno.config(text="Увеличить окно")
         t=0
 def kit():
+        x1=np.arange(0,9.5,0.5)#min max step
+    y1=(2/27)*x1*x1-3 #dark blue line
+    x2=np.arange(-10,0.5,0.5)#min max step
+    y2=0.04*x2*x2-3 #orange line
+    x3=np.arange(-9,-2.5,0.5)#min max step
+    y3=(2/9)*(x3+6)**2+1#green line
+    x4=np.arange(-3,9.5,0.5)#min max step
+    y4=(-1/12)*(x4-3)**2+6 #red line
+    x5=np.arange(5,9,0.5)#min max step
+    y5=(1/9)*(x5-5)**2+2#purple line
+    x6=np.arange(5,8.3,0.5)#min max step
+    y6=(1/8)*(x6-7)**2+1.5#brown line
+    x7=np.arange(-13,-8.5,0.5)#min max step
+    y7=(-0.75)*(x7+11)**2+6
+    x8=np.arange(-15,-12.5,0.5)#min max step
+    y8=(-0.5)*(x8+13)**2+3
+    x9=np.arange(-15,-9.5,0.5)#min max step
+    y9=[1]*len(x9)
+    x10=np.arange(3,4,0.5)#
+    y10=[3]*len(x10)
+    fig=plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
+    plt.title('square equation')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+def o4ki():
+    x1=np.arange(-9,-1,0.5)
+    y1=(-1/16)*(x1+5)**2+2
+    x2=np.arange(1,9,0.5)
+    y2=(-1/16)*(x2-5)**2+2
+    x3=np.arange(-9,-1,0.5)
+    y3=1/4*(x3+5)**2-3
+    x4=np.arange(1,9,0.5)
+    y4=1/4*(x4-5)**2-3
+    x5=np.arange(-9,-6,0.5)
+    y5=-(x5+7)**2+5
+    x6=np.arange(6,9,0.5)
+    y6=-(x6-7)**2+5
+    x7=np.arange(-1,1,0.5)
+    y7=(-0.5)*x7*x7+1.5
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title('Очки')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+def babo4():
     x1=np.arange(0,9,0.5)
     y1=(2/27)*x1*x1-3
     x2=np.arange(-10,0,0.5)
@@ -93,10 +143,6 @@ def kit():
     plt.xlabel("x")
     plt.grid(True)
     plt.show()
-def zont():
-    pass
-def babo4():
-    pass
 aken=Tk()
 aken.title("Решение квадратного уравнения")
 aken.geometry("800x300")
@@ -128,8 +174,8 @@ uvokno=Button(f2,text="Увеличить окно", font="Arial 20",bg="green",
 uvokno.pack()
 kit=Radiobutton(f2,text="Кит",font="Arial 20",bg="blue",command=kit)
 kit.pack()
-zont=Radiobutton(f2,text="Зонтик",font="Arial 20",bg="blue",command=zont)
-zont.pack()
+o4ki=Radiobutton(f2,text="очки",font="Arial 20",bg="blue",command=o4ki)
+o4ki.pack()
 babo4=Radiobutton(f2,text="Бабочка",font="Arial 20",bg="blue",command=babo4)
 babo4.pack()
 aken.mainloop()
